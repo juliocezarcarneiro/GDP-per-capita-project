6dac207327fd8c0e46ca1e573f451c59bdc32019
juliocezarcarneiro <carneiro.julio@icloud.com>
2025-04-10 21:02:23 -0400

    Debugging

--- bubble-charts/static/js/app.js
+++ bubble-charts/static/js/app.js
@@ -1,126 +1,52 @@
-// Build the metadata panel
-function buildMetadata(sample) {
-  d3.json("https://juliocezarcarneiro.github.io/GDP-per-capita-project/bubble-charts/countries-data.json").then((data) => {
-
-// get the metadata field
-let metadata = data.metadata;
-
-    // Filter the metadata for the object with the desired sample number
-    let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    let result = resultArray[0];
-
-    // Use d3 to select the panel with id of `#sample-metadata`
-    let PANEL = d3.select("#sample-metadata");
-
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
-
-  });
-}
-
-// function to build both charts
-function buildCharts(sample) {
-  d3.json("https://juliocezarcarneiro.github.io/GDP-per-capita-project/bubble-charts/countries-data.json").then((data) => {
-
-    // Get the samples field
-    let samples = data.samples;
-
-    // Filter the samples for the object with the desired sample number
-    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    let result = resultArray[0];
-
-    // Get the otu_ids, otu_labels, and sample_values
-    let GDP_per_capita = result.otu_ids;
-    let Health_spending_per_capita = result.otu_labels;
-    let sample_values = result.sample_values;
-
-    // Build a Bubble Chart
-    let bubbleLayout = {
-      title: "GDP per Capita vs. Health Spending",
-      hovermode: "closest",
-      xaxis: { title: "GDP" },
-      margin: { t: 30 }
-    };
-
-    let bubbleData = [{
-      x: GDP_per_capita
-      y: Health_spending_per_capita,
-      text: otu_labels,
-      mode: "markers",
-      marker: {
-        size: sample_values,
-        color: otu_ids,
-        colorscale: "Earth"
-      }
-    }];
-
-    // Render the Bubble Chart
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
-    let barData = [{
-      y: yticks,
-      x: sample_values.slice(0, 10).reverse(),
-      text: otu_labels.slice(0, 10).reverse(),
-      type: "bar",
-      orientation: "h"
-    }];
-
-    let barLayout = {
-      title: "Top 10 Bacteria Cultures Found",
-      margin: { t: 30, l: 150 }
-    };
-
-
-    // Render the Bar Chart
-    Plotly.newPlot("bar", barData, barLayout);
-  });
-}
-
-// Function to run on page load
+// Initialize the page
 function init() {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+  buildCharts();
+}
 
-    // Get the names field
-    let sampleNames = data.names;
+// Build the bubble chart
+function buildCharts() {
+  d3.json("https://juliocezarcarneiro.github.io/GDP-per-capita-project/bubble-charts/countries-data.json")
+    .then((data) => {
+      let countries = data.map(d => d["Country Name"]);
+      let gdp = data.map(d => d.gdp_per_capita);
+      let health = data.map(d => d.health_spending);
 
-    // Use d3 to select the dropdown with id of `#selDataset`
-    let dropdown = d3.select("#selDataset");
+      let bubbleData = [{
+        x: gdp,
+        y: health,
+        text: countries.map((c, i) => `${c}<br>GDP: $${gdp[i]}<br>Health: $${health[i]}`),
+        mode: 'markers',
+        marker: {
+          size: health.map(h => Math.sqrt(h) / 2),
+          sizemode: 'area',
+          color: 'earth'
+        },
+      }];
 
-    // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-    sampleNames.forEach((sample) => {
-      dropdown.append("option")
-        .text(sample)
-        .property("value", sample);
+      const bubbleLayout = {
+        title: 'GDP per Capita vs Health Spending per Capita by Country',
+        xaxis: {
+          title: 'GDP per Capita (USD) - Log Scale',
+          type: 'log',
+          zeroline: false
+        },
+        yaxis: {
+          title: 'Health Spending per Capita (USD) - Log Scale',
+          type: 'log',
+          zeroline: false
+        },
+        hovermode: 'closest',
+        showlegend: false,
+        margin: { t: 50, l: 80, r: 50, b: 80 },
+        height: 600
+      };
+
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    })
+    .catch(error => {
+      console.error("Error loading the data: ", error);
     });
-
-    // Get the first sample from the list
-    let firstSample = sampleNames[0];
-
-    // Build charts and metadata panel with the first sample
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
 }
 
-// Function for event listener
-function optionChanged(newSample) {
-  // Build charts and metadata panel each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
-
-// Initialize the dashboard
-init();
+// Call init when the page loads
+window.onload = init;
